bebbb4eac9e54a1f9b8fa28025a46cee88e648d7
Samantha Fok <samantha.fok@hotmail.com>
2021-01-05 20:06:42 +0000

    Found sum and average of all stores annual revenue, and num of stores with annual revenue > 1M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@ar_sum = Store.sum(:annual_revenue)
+@ar_avg = Store.average(:annual_revenue)
+
+puts @ar_sum #4114000
+puts @ar_avg #822800.0
+
+@ar_count = Store.where("annual_revenue > ?", 1000000).count
+
+p @ar_count #2
