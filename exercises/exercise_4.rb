6466bffd64dac3975aed34de73b53a742085ac37
Samantha Fok <samantha.fok@hotmail.com>
2021-01-05 19:11:48 +0000

    Found all stores that carry mens apparel

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,13 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true, created_at: Time.now)
+
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false, created_at: Time.now)
+
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true, created_at: Time.now)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+p @mens_stores 
+# store1, store5, store6
